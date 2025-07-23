d40cdd43b90a06bb51af27772533dbb8d8e73f68
Tarishi-afk <Tarishi@anavcloudsoftwares.com>
2025-07-23 17:32:22 +0530

    Add files via upload

---
+++ CAMP_1/app/input.py
@@ -0,0 +1,32 @@
+from datetime import *
+time = datetime.now().date()
+##################################################################################################
+batch_size = '2'
+batch_delay = '180'
+campaign_regions = ""   ##### USA, Israel etc.
+campaign_topic = "I"
+subsequence = "AI"        ##### Email-1, Follow-up 1, Follow-up 2, Follow-up 3
+start_time_ist = '15:00'   ##### 24-hour format
+end_time_ist = '20:00'      ##### 24-hour format
+subject = "Let's Solve Your Salesforce Challenges with Expert Solutions"      ## Subject for mail
+date = time
+body = """
+     I hope you’re doing well.  
+I am reaching out to introduce AnavClouds Software Solutions. We’re a Salesforce Certified Partner based in the USA. With years of experience helping organizations like yours optimise their Salesforce platforms. Our specialty is offering Salesforce consulting services that are specifically suited to your company's requirements. 
+
+ 
+
+To help you get the most out of your Salesforce environment, we offer a variety of engagement models, including infinite development. Our professional team collaborates with you to realise the issues you're confronting and develop a Salesforce plan that best represents your company's objectives. 
+
+ 
+
+I would love to know your needs if you are willing to communicate, and how we can improve your Salesforce experience. We promise support services to keep everything functioning properly and a five-hour response time in any time zone. 
+
+ 
+
+Could we arrange a brief 20-minute call at your convenience to talk about your Salesforce difficulties and see how we can help you? """               ## Body for mail
+
+footnote = "Looking forward to hearing from you!"
+time_zone = "Asia/Kolkata"
+
+
